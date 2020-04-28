32d241521576ab2a9a16237b06253a9123c84a8a
Perre <Perre@users.noreply.github.com>
2020-04-28 12:28:45 +0200

    Removed unncescary fetch and get code from server.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //const GeoTIFF = require('geotiff');
 const express = require('express');
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 //const fs = require('fs');
 const app = express();
 console.log('starting TP feed server');
@@ -11,7 +11,8 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '1Gb' }));
 
 
-
+/*
 app.get('/data', async (request, response) => {
     response.json({data:''});
 });
+*/
